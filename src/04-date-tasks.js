1bebd6b816b9f340406fd0029409ddeb9a72f891
Vladimir <admin@ttp.by>
2022-07-24 22:44:12 +0300

    feat: Resolv one datetime task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -73,8 +73,14 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let diffTime = (endDate - startDate);
+  const hours = Math.floor(diffTime / (60 * 60 * 1000));
+  diffTime -= hours * 60 * 60 * 1000;
+  const minuts = Math.floor(diffTime / (60 * 1000));
+  diffTime -= minuts * 60 * 1000;
+  const seconds = Math.floor(diffTime / 1000);
+  return `${hours.toString().padStart(2, '0')}:${minuts.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${(diffTime % 1000).toString().padStart(3, '0')}`;
 }
 
 
