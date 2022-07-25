ff7f2b81294c9da482284ec6d8814f8e83f4b968
Vladimir <admin@ttp.by>
2022-07-25 23:14:09 +0300

    feat: Resolve getComposition

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(...args) {
+  return (x) => args.reduceRight((acc, f) => f(acc), x);
 }
 
 
