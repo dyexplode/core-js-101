1b990281529aa20346c1a4d2beca2fc2ad2d6dc1
Vladimir <admin@ttp.by>
2022-07-26 23:15:14 +0300

    feat: Resolve matrix multipluer

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -448,8 +448,20 @@ function getCommonDirectoryPath(pathes) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const result = [];
+  const filler = [];
+  filler.length = m2[0].length;
+  filler.fill(0);
+  m1.forEach(() => result.push([...filler]));
+  for (let i = 0; i < m1.length; i += 1) {
+    for (let j = 0; j < m2[0].length; j += 1) {
+      for (let n = 0; n < m1[0].length; n += 1) {
+        result[i][j] += m1[i][n] * m2[n][j];
+      }
+    }
+  }
+  return result;
 }
 
 
